b97736fd0e66f3afaaf9f411e3e48c37ddea4c10
johnnydee8 <j.dworzynski@me.com>
2016-10-25 18:38:51 +0100

    connection to db established, first data saved in messages view controller

--- Turfy/Turfy/MessagesViewController.swift
+++ Turfy/Turfy/MessagesViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Firebase
 
 class MessagesViewController: UIViewController {
-
+    
+    let ref = FIRDatabase.database().reference(withPath: "messages")
+    
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+        // The code below saves shit in the DB, created a field 'lastTested' and assigns a value 'testing_lalala' to it
+        let testItemRef = self.ref.child("lastTested")
+        testItemRef.setValue("testing_lalala")
 	}
 
 	override func didReceiveMemoryWarning() {
