49ecec4ccc6b8d3d4b358d96fb8ef02f6fb1f205
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-10-25 11:27:40 +0100

    let it begin!

---
+++ Turfy/Turfy/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Turfy
+//
+//  Created by Lawrence Dawson on 25/10/2016.
+//  Copyright © 2016 Lawrence Dawson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
