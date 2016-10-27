866210acf4a88146aaa7bf8f45ab9b004eeac4b9
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-10-27 14:38:46 +0100

    we can finally look inside an object returned from a DB

--- Turfy/Turfy/MessagesViewController.swift
+++ Turfy/Turfy/MessagesViewController.swift
@@ -10,29 +10,40 @@ import UIKit
 import Firebase
 
 class MessagesViewController: UIViewController {
-    
-    let record = FIRDatabase.database().reference(withPath: "messages/james")
+	let ref = FIRDatabase.database().reference().child("messages")
+
+	func retrieveMessageAttributes() {
+		ref.queryOrderedByKey().queryEqual(toValue: "-KV4T3PQzUuNuOKkjEX1").observe(.value, with: { (snapshot) in
+			print(snapshot)
+			for item in snapshot.children {
+				let data = (item as! FIRDataSnapshot).value! as! NSDictionary
+				print("*********************")
+				print((data["text"])!)
+			}
+		})
+	}
+	
     
     func saveData() {
         let message = Message(id: "1", sender: "James", recipient: "Lawrence", location: "Makers Academy", text: "Hey James is awesome!", radius: 30)
-        let itemRef = self.record.childByAutoId()
+        let itemRef = self.ref.childByAutoId()
         itemRef.setValue(message.toAnyObject())
-        
+		
 
     }
     
     func printData() {
-        record.observe(.value, with: { snapshot in
-            print(snapshot.value)
+        ref.observe(.value, with: { snapshot in
+			 print(snapshot.value)
         })
 
     }
     
 	override func viewDidLoad() {
 		super.viewDidLoad()
-        saveData()
-        print(printData())
-		
+//        saveData()
+//        print(printData())
+		retrieveMessageAttributes()
 	}
 
 	override func didReceiveMemoryWarning() {
