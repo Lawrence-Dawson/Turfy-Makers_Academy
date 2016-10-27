6c291fe13f10e7d1f55375fcea9cbd4b906fd322
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-10-27 10:44:03 +0100

    deleted a few comments

--- Turfy/Turfy/MessagesViewController.swift
+++ Turfy/Turfy/MessagesViewController.swift
@@ -15,10 +15,8 @@ class MessagesViewController: UIViewController {
     
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-        // The code below saves shit in the DB, created a field 'lastTested' and assigns a value 'testing_lalala' to it
-//        let testItemRef = self.ref.child("lastTested")
-//        testItemRef.setValue("testing_lalala")
+		
+//
 		let message = Message(id: "1", sender: "Johnny", recipient: "Lawrence", location: "Makers Academy", text: "Hey Lawrence you're the best guy in the world!", radius: 30)
 		let itemRef = self.ref.childByAutoId()
 		itemRef.setValue(message.toAnyObject())
@@ -27,15 +25,6 @@ class MessagesViewController: UIViewController {
 			print(snapshot.value)
 			})
 		
-//
-		
-        
-//            self.ref.observeSingleEventOfType(.value, withBlock: { (snapshot) in
-//            
-//            if !snapshot.exists() { return }
-//            print(snapshot)
-//            
-//        })
 	}
 
 	override func didReceiveMemoryWarning() {
