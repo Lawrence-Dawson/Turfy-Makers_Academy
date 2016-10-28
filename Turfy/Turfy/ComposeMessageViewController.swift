01c4e682048bc4e0f67295842d3c5355f5153b7c
Joseph <joseph.jr@gmail.com>
2016-10-28 18:10:48 +0100

    resolve conflicts

--- Turfy/Turfy/ComposeMessageViewController.swift
+++ Turfy/Turfy/ComposeMessageViewController.swift
@@ -30,6 +30,14 @@ class ComposeMessageViewController: UIViewController {
     }
     
 
+    @IBOutlet weak var messageTitle: UITextField!
+    
+    @IBOutlet weak var messageBody: UITextView!
+    @IBAction func submitMessage(_ sender: AnyObject) {
+        message = Message.
+    }
+    
+    
     /*
     // MARK: - Navigation
 
