721566838a78c3853183df2213f260c6cc33d65e
johnnydee8 <j.dworzynski@me.com>
2016-10-29 15:11:39 +0100

    geofencing works

--- Turfy/Turfy/Utilities.swift
+++ Turfy/Turfy/Utilities.swift
@@ -12,7 +12,7 @@ import UIKit
 extension UIViewController {
     func showAlert(withTitle title: String?, message: String?) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        let action = UIAlertAction(title: "Dismiss", style: .cancel, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
