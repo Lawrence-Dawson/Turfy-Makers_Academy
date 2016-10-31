d2fb73eae428a584a626a8c32d35b400b17fa25d
Tam <tamborinehands@gmail.com>
2016-10-31 18:28:36 +0000

    still ui not working

---
+++ Turfy/Turfy/CustomOneCell.swift
@@ -0,0 +1,25 @@
+//
+//  CustomOneCell.swift
+//  Turfy
+//
+//  Created by Tam Borine on 31/10/2016.
+//  Copyright © 2016 Lawrence Dawson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RecipientSearchTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var alabel: UILabel!
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
