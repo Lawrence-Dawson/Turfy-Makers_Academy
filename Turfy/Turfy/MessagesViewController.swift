57e1ff62f6fbf0cf4b83aa2bf60657fbc1f2feae
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-10-31 15:22:58 +0000

    message class extracted

--- Turfy/Turfy/MessagesViewController.swift
+++ Turfy/Turfy/MessagesViewController.swift
@@ -12,81 +12,34 @@ import Firebase
 
 class MessagesViewController: UIViewController {
 	
-	let uid = (FIRAuth.auth()?.currentUser?.uid)!
-    let ref = FIRDatabase.database().reference().child("messages")
-    let inboxRef = FIRDatabase.database().reference().child("messages").child((FIRAuth.auth()?.currentUser?.uid)!)
-	let sampleMessage : Message = Message(id: "test message", sender: (FIRAuth.auth()?.currentUser?.uid)!, recipient: "zwcxlPQwDAhYIxX9k4hDn77LvQY2", text: "Hey Johnny!", latitude: 50.00, longitude: 0.00, radius: 500, eventType: "On Entry")
+//	let uid = (FIRAuth.auth()?.currentUser?.uid)!
+//    let ref = FIRDatabase.database().reference().child("messages")
+//    let inboxRef = FIRDatabase.database().reference().child("messages").child((FIRAuth.auth()?.currentUser?.uid)!)
 	
-    let dateformatter = DateFormatter()
+//	let sampleMessage : Message = Message(id: "test message", sender: (FIRAuth.auth()?.currentUser?.uid)!, recipient: "zwcxlPQwDAhYIxX9k4hDn77LvQY2", text: "Hey Johnny!", latitude: 50.00, longitude: 0.00, radius: 500, eventType: "On Entry")
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		saveData(message: sampleMessage)
-       // saveData(id: "test message", sender: "Johnny", recipient: "Lawrence", text: "This app is going to be great!", latitude: 50.00, longitude: 0.00 , radius: 500, eventType: "On Entry")
-        inboxRef.observe(.childAdded, with: { (snapshot) -> Void in
-            print(snapshot)
-			
-			let message = Message(snapshot: snapshot)
-			print(message.toAnyObject())
-			//addNewMessage(message: message)
-        })
-		
+//		saveData(message: sampleMessage)
+//		
+//        inboxRef.observe(.childAdded, with: { (snapshot) -> Void in
+//            print(snapshot)
+//			
+//			let message = Message(snapshot: snapshot)
+//			print(message.toAnyObject())
+//			//addNewMessage(message: message)
+//        })
+//		
 	}
-
-	
-
-
-
-
-	//func retrieveMessageAttributes(messageID: String) {
-	//	inboxRef.queryOrderedByKey().queryEqual(toValue: messageID).observe(.value, with: { (snapshot) in
-	//		print(snapshot)
-	//		for item in snapshot.children {
-	//			let data = (item as! FIRDataSnapshot).value! as! NSDictionary
-                
-     //           let id = messageID
-     //           let sender = (data["sender"])!
-     //           let recipient = (data["recipient"])!
-     //           let text = (data["text"])!
-     //           let latitude = (data["latitude"])!
-     //           let longitude = (data["longitude"])!
-     //           let radius = (data["radius"])!
-      //          let eventType = (data["eventType"])!
-        
-                
-	//			let sentAtStr = (data["sentAt"])!
-	//			self.dateformatter.dateFormat = "dd/MM/yy h:mm"
-	//			let sentAtDate = self.dateformatter.date(from: sentAtStr as! String)
-	//			self.dateformatter.dateFormat = "dd/MM/yy h:mm"
-	//			let sentAt = self.dateformatter.string(from: sentAtDate!)
-      //
-        //        let expiresAtStr = (data["expires"])!
-         //       let expiresDate = self.dateformatter.date(from: expiresAtStr as! String)
-          //      self.dateformatter.dateFormat = "dd/MM/yy h:mm"
-           //     let expires = self.dateformatter.string(from: expiresDate!)
-                
-           //     print(id)
-			//	print(sender)
-		//		print(recipient)
-	//			print(text)
-	//			print(radius)
-	//			print(sentAt)
-      //          print(latitude)
-        //        print(longitude)
-          //      print(eventType)
-           //     print(expires)
-	//		}
-	//	})
-//	}
-	
     
-	func saveData(message: Message) {
-
-		let recipient: String = message.recipient
-		let messageContent = message.toAnyObject()
-        
-        let itemRef = self.ref.child(recipient).childByAutoId()
-        itemRef.setValue(messageContent)
-    }
+//	func saveData(message: Message) {
+//
+//		let recipient: String = message.recipient
+//		let messageContent = message.toAnyObject()
+//        
+//        let itemRef = self.ref.child(recipient).childByAutoId()
+//        itemRef.setValue(messageContent)
+//    }
 	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
