3f9f247d36e03e854efa528ae80498ad6a12cccd
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-10-31 13:20:09 +0000

    messages between users works in the console, now needs geotification hook up

--- Turfy/Turfy/MessagesViewController.swift
+++ Turfy/Turfy/MessagesViewController.swift
@@ -9,22 +9,27 @@
 import UIKit
 import Firebase
 
+
 class MessagesViewController: UIViewController {
-    
+	
+	let uid = (FIRAuth.auth()?.currentUser?.uid)!
     let ref = FIRDatabase.database().reference().child("messages")
     let inboxRef = FIRDatabase.database().reference().child("messages").child((FIRAuth.auth()?.currentUser?.uid)!)
+	let sampleMessage : Message = Message(id: "test message", sender: (FIRAuth.auth()?.currentUser?.uid)!, recipient: "zwcxlPQwDAhYIxX9k4hDn77LvQY2", text: "Hey Johnny!", latitude: 50.00, longitude: 0.00, radius: 500, eventType: "On Entry")
+	
     let dateformatter = DateFormatter()
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		//retrieveMessageAttributes(messageID: "-KVA7xvDvdTQ61GMDyST")
-        saveData(id: "test message", sender: "Johnny", recipient: "Lawrence", text: "This app is going to be great!", latitude: 50.00, longitude: 0.00 , radius: 500, eventType: "On Entry")
+		saveData(message: sampleMessage)
+       // saveData(id: "test message", sender: "Johnny", recipient: "Lawrence", text: "This app is going to be great!", latitude: 50.00, longitude: 0.00 , radius: 500, eventType: "On Entry")
         inboxRef.observe(.childAdded, with: { (snapshot) -> Void in
-            //self.comments.append(snapshot)
-            //self.tableView.insertRows(at: [IndexPath(row: self.comments.count-1, section: self.kSectionComments)], with: UITableViewRowAnimation.automatic)
-            //self.retrieveMessageAttributes(messageID: snapshot)
             print(snapshot)
+			
+			let message = Message(snapshot: snapshot)
+			print(message.toAnyObject())
+			//addNewMessage(message: message)
         })
-        
+		
 	}
 
 	
@@ -32,54 +37,55 @@ class MessagesViewController: UIViewController {
 
 
 
-	func retrieveMessageAttributes(messageID: String) {
-		inboxRef.queryOrderedByKey().queryEqual(toValue: messageID).observe(.value, with: { (snapshot) in
-			print(snapshot)
-			for item in snapshot.children {
-				let data = (item as! FIRDataSnapshot).value! as! NSDictionary
+	//func retrieveMessageAttributes(messageID: String) {
+	//	inboxRef.queryOrderedByKey().queryEqual(toValue: messageID).observe(.value, with: { (snapshot) in
+	//		print(snapshot)
+	//		for item in snapshot.children {
+	//			let data = (item as! FIRDataSnapshot).value! as! NSDictionary
                 
-                let id = messageID
-                let sender = (data["sender"])!
-                let recipient = (data["recipient"])!
-                let text = (data["text"])!
-                let latitude = (data["latitude"])!
-                let longitude = (data["longitude"])!
-                let radius = (data["radius"])!
-                let eventType = (data["eventType"])!
+     //           let id = messageID
+     //           let sender = (data["sender"])!
+     //           let recipient = (data["recipient"])!
+     //           let text = (data["text"])!
+     //           let latitude = (data["latitude"])!
+     //           let longitude = (data["longitude"])!
+     //           let radius = (data["radius"])!
+      //          let eventType = (data["eventType"])!
         
                 
-				let sentAtStr = (data["sentAt"])!
-				self.dateformatter.dateFormat = "dd/MM/yy h:mm"
-				let sentAtDate = self.dateformatter.date(from: sentAtStr as! String)
-				self.dateformatter.dateFormat = "dd/MM/yy h:mm"
-				let sentAt = self.dateformatter.string(from: sentAtDate!)
+	//			let sentAtStr = (data["sentAt"])!
+	//			self.dateformatter.dateFormat = "dd/MM/yy h:mm"
+	//			let sentAtDate = self.dateformatter.date(from: sentAtStr as! String)
+	//			self.dateformatter.dateFormat = "dd/MM/yy h:mm"
+	//			let sentAt = self.dateformatter.string(from: sentAtDate!)
+      //
+        //        let expiresAtStr = (data["expires"])!
+         //       let expiresDate = self.dateformatter.date(from: expiresAtStr as! String)
+          //      self.dateformatter.dateFormat = "dd/MM/yy h:mm"
+           //     let expires = self.dateformatter.string(from: expiresDate!)
                 
-                let expiresAtStr = (data["expires"])!
-                let expiresDate = self.dateformatter.date(from: expiresAtStr as! String)
-                self.dateformatter.dateFormat = "dd/MM/yy h:mm"
-                let expires = self.dateformatter.string(from: expiresDate!)
-                
-                print(id)
-				print(sender)
-				print(recipient)
-				print(text)
-				print(radius)
-				print(sentAt)
-                print(latitude)
-                print(longitude)
-                print(eventType)
-                print(expires)
-			}
-		})
-	}
+           //     print(id)
+			//	print(sender)
+		//		print(recipient)
+	//			print(text)
+	//			print(radius)
+	//			print(sentAt)
+      //          print(latitude)
+        //        print(longitude)
+          //      print(eventType)
+           //     print(expires)
+	//		}
+	//	})
+//	}
 	
     
-    func saveData(id: String, sender: String, recipient: String, text: String, latitude: Double, longitude: Double, radius: Double, eventType: String, expires: Int = 2) {
+	func saveData(message: Message) {
 
-        let message = Message(id: id, sender: sender, recipient: recipient, text: text, latitude: latitude, longitude: longitude, radius: radius, eventType: eventType, expires: expires)
+		let recipient: String = message.recipient
+		let messageContent = message.toAnyObject()
         
-        let itemRef = self.ref.childByAutoId()
-        itemRef.setValue(message.toAnyObject())
+        let itemRef = self.ref.child(recipient).childByAutoId()
+        itemRef.setValue(messageContent)
     }
 	
 	override func didReceiveMemoryWarning() {
