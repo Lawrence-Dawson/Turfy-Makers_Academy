81ceba27edc95506faa33f5d847b6bedec9dac8f
Joseph <joseph.jr@gmail.com>
2016-11-02 13:02:22 +0000

    add map pin feature to single message

--- Turfy/Turfy/SingleMessageViewController.swift
+++ Turfy/Turfy/SingleMessageViewController.swift
@@ -8,13 +8,64 @@
 
 import UIKit
 import MapKit
+
 class SingleMessageViewController: UIViewController {
 
+    let regionRadius: CLLocationDistance = 500
+    
+    var searchController:UISearchController!
+    var annotation:MKAnnotation!
+    var localSearchRequest:MKLocalSearchRequest!
+    var localSearch:MKLocalSearch!
+    var localSearchResponse:MKLocalSearchResponse!
+    var error:NSError!
+    var pointAnnotation:MKPointAnnotation!
+    var pinAnnotationView:MKPinAnnotationView!
+    
+    func centerMapOnLocation(location: CLLocationCoordinate2D) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordMessage!, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+
+
+    var textMessage = ""
+    var coordMessage: CLLocationCoordinate2D? = nil
+  //  var messageObject["latitude"]: Int
+ //   messageObject["latitude"] = 50
+ //   var messageObject.longitude = 50
     @IBOutlet weak var messageTitle: UILabel!
-    @IBOutlet weak var messageBody: UITextView!
     @IBOutlet weak var mapView: MKMapView!
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? Pin {
+            let identifier = "pin"
+            var view: MKPinAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+                as? MKPinAnnotationView {
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            } else {
+                
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure) as UIView
+            }
+            return view
+        }
+        return nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageTitle.text = textMessage
+        
+        
+        let initialLocation = coordMessage
+        centerMapOnLocation(location: initialLocation!)
+
+        let pin = Pin(title: "London", locationName: "Current Location", discipline: "Location", coordinate: coordMessage!)
+        mapView.addAnnotation(pin)
 
         // Do any additional setup after loading the view.
     }
