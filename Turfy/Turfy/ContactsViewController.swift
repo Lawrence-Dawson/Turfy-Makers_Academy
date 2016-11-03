8d58005377ea4378061511b8e18f1a78da01869d
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-11-03 11:48:46 +0000

    half way through

--- Turfy/Turfy/ContactsViewController.swift
+++ Turfy/Turfy/ContactsViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Firebase
+
 
 class ContactsViewController: UIViewController {
-
+	
+	var usersArray = [User]
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,5 +34,36 @@ class ContactsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+	
+	func loadUsers(){
+		
+		let usersRef = FIRDatabase.database().reference().child("user")
+		
+		usersRef.observeSingleEvent(of: .value, with: { (snapshot) in
+			
+			var allUsers = [User]()
+			
+			for user in snapshot.children {
+				
+				let newUser = User(snapshot: user as! FIRDataSnapshot)
+				allUsers.append(newUser)
+			
+			}
+//			usersArray.sortInPlace {(user1:User, user2:User) -> Bool in
+//				user.name < user.name
+//			})
+			
+//			self.usersArray = allUsers.sort(by: { (user1, user2) -> Bool in
+//				user1.name < user2.name
+//			})
+			
+			self.tableView.reloadData()
+			
+			
+			
+		}) { (error) in
+			print("Error Loading contacts from Firebase")
+	}
 
+}
 }
